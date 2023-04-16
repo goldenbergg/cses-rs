1435fd456e129052f3e632cb0a6efa14a3101a3c
goldenbergg <ggoldenb@purdue.edu>
2023-04-15 22:36:53 -0500

    Completed perms.rs

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ CSES Problem Set in the Rust Programming Language
 - [x] [Missing Number](https://github.com/goldenbergg/cses-rs/blob/master/src/ms_num.rs)
 - [x] [Repetitions](https://github.com/goldenbergg/cses-rs/blob/master/src/repetitions.rs)
 - [x] [Increasing Array](https://github.com/goldenbergg/cses-rs/blob/master/src/incr_arr.rs)
-- [ ] [Permutations](https://github.com/goldenbergg/cses-rs/blob/master/src/perms.rs)
+- [x] [Permutations](https://github.com/goldenbergg/cses-rs/blob/master/src/perms.rs)
 - [ ] Number Spiral
 - [ ] Two Knights
 - [ ] Two Sets
--- src/perms.rs
+++ src/perms.rs
@@ -26,7 +26,56 @@ macro_rules! ls2vec {
     };
 }
 
-#[allow(unused_variables)]
+use std::collections::HashSet;
+
 fn main() {
     let n: i64 = input!().parse().unwrap();
+
+    for i in 1..(n + 1) {
+        let mut inc = 2;
+        let mut seen: HashSet<i64> = HashSet::new();
+        let mut perm: Vec<i64> = Vec::new();
+        let mut sum: i64 = 0;
+
+        let mut curr_num: i64 = i;
+        seen.insert(curr_num);
+        sum += curr_num;
+        perm.push(curr_num);
+
+        while sum < (n * (n + 1) / 2) {
+            let mut temp = curr_num - inc;
+
+            if inc == n {
+                break
+            } else if temp <= 0 || seen.contains(&temp) == true {
+                temp = curr_num + inc;
+
+                if temp > n || seen.contains(&temp) == true {
+                    inc += 1;
+                } else {
+                    curr_num += inc;
+                    sum += curr_num;
+                    seen.insert(curr_num);
+                    perm.push(curr_num);
+                    inc = 2;
+                }
+            } else {
+                curr_num -= inc;
+                sum += curr_num;
+                seen.insert(curr_num);
+                perm.push(curr_num);
+                inc = 2;
+            }
+        }
+
+        if sum == (n * (n + 1) / 2) {
+            for x in perm.iter() {
+                print!("{} ", x);
+            }
+
+            return
+        }
+    }
+
+    print!("NO SOLUTION");
 }
