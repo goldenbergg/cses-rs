256127a8a73761cec97e6ab6dc5bc03e43e4fece
goldenbergg <ggoldenb@purdue.edu>
2023-05-08 20:57:46 -0500

    Completed max_subarr_sum.rs

--- README.md
+++ README.md
@@ -30,3 +30,4 @@ CSES Problem Set in the Rust Programming Language
 - [x] [Restaurant Customers](https://github.com/goldenbergg/cses-rs/blob/master/src/sorting_and_searching/rest_custs.rs)
 - [x] [Movie Festival](https://github.com/goldenbergg/cses-rs/blob/master/src/sorting_and_searching/movie_fest.rs)
 - [x] [Sum of Two Values](https://github.com/goldenbergg/cses-rs/blob/master/src/sorting_and_searching/sum_of_two_vals.rs)
+- [x] [Maximum Subarray Sum](https://github.com/goldenbergg/cses-rs/blob/master/src/sorting_and_searching/max_subarr_sum.rs)
---
+++ src/sorting_and_searching/max_subarr_sum.rs
@@ -0,0 +1,73 @@
+#[macro_export]
+macro_rules! input {
+    () => {
+        {   
+            use std::io;
+            let mut buf = String::new();
+            let stdin = io::stdin();
+            stdin.read_line(&mut buf).expect("unable to read line");
+            buf.strip_suffix("\r\n")
+                .or(buf.strip_suffix("\n"))
+                .unwrap_or(&buf)
+                .to_string()
+        }   
+    };
+}
+ 
+#[macro_export]
+macro_rules! ls2vec {
+    ($x:expr) => {
+        {
+            let ls = $x.to_string();
+            ls.split(" ")
+                .map(|x| x.parse().unwrap())
+                .collect()
+        }
+    };
+}
+ 
+#[allow(dead_code)]
+fn nck(n: u64, k: u64) -> u64 {
+    if k == 0 {
+        return 1
+    } else if k > n / 2 {
+        return nck(n, n - k)
+    } else {
+        return n * nck(n - 1, k - 1) / k
+    }
+}
+ 
+#[allow(dead_code)]
+fn next_permutation(v: &mut Vec<u8>) -> bool {
+    if let Some((i, k)) = v
+        .windows(2)
+        .enumerate()
+        .rfind(|(_, w)| w[0] < w[1])
+        .map(|(j, w)| {
+            let k = v[j + 1..].iter().rposition(|x| w[0] < *x).unwrap_or(0);
+            (j, k)
+        })
+    {
+        v.swap(i, k + i + 1);
+        v[i + 1..].reverse();
+        return true;
+    }
+ 
+    false
+}
+
+use std::cmp::max;
+
+fn main() {
+    let _n: usize = input!().parse().unwrap();
+    let x_n: Vec<i64> = ls2vec!(input!());
+    let mut glbl_max: i64 = i64::MIN;
+    let mut sum: i64 = 0;
+
+    for x in x_n {
+        sum = max(x, sum + x);
+        glbl_max = max(glbl_max, sum);
+    }
+
+    println!("{}", glbl_max);
+}
